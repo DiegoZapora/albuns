7c976be065a961aee18249c744c94d55220d879c
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-05-18 10:48:52 -0300

    chega

--- script/index.js
+++ script/index.js
@@ -3,13 +3,83 @@ const res = document.getElementById("res")
 const a = document.getElementById("a")
 const img = document.getElementById("img")
 
-let album = ["imagens/angels.jpg", "imagens/fire.jpg", "imagens/holy.jpg", "imagens/mentalize.jpeg", "imagens/paradisum.jpg", "imagens/reason.jpg", "imagens/ritual.jpg", "imagens/ritualive.jpg", "imagens/soldiers.jpg", "imagens/theatre.jpg", "imagens/theturn.jpg", "imagens/time.jpg", "imagens/virgo.jpg", "imagens/tolive.jpg"]
+let album = [
+    {
+       nome: "Algels Cry", 
+       img: "imagens/angels.jpg"
+    },
+
+    {
+       nome: "Fireworks",
+       img: "imagens/fire.jpg"
+    },
+
+    {
+       nome: "Holy Land",
+       img: "imagens/holy.jpg"
+    },
+
+    {
+       nome: "Mentalize",
+       img: "imagens/mentalize.jpeg",
+    },
+
+    {
+       nome: "In Paradisum",
+       img: "imagens/paradisum.jpg",
+        
+    }, 
+    {
+       nome: "Reason",
+       img: "imagens/reason.jpg",
+    },
+
+    {
+       nome: "Ritual",
+       img: "imagens/ritual.jpg",
+    },
+
+    {
+       nome: "Ritualive",
+       img: "imagens/ritualive.jpg", 
+    },
+
+    {
+       nome: "Soldiers of Sunrise",
+       img: "imagens/soldiers.jpg",
+    },
+
+    {
+       nome: "Theatre of Fate",
+       img: "imagens/theatre.jpg",
+    },
+
+    {
+       nome: "The Turn of The Lights",
+       img: "imagens/theturn.jpg",
+    },
+
+    {
+       nome: "Time to be Free",
+       img: "imagens/time.jpg",
+    },
+
+    {
+       nome: "Virgo",
+       img: "imagens/virgo.jpg", 
+    },
+    
+    {
+       nome: "To live Again Live",
+       img: "imagens/tolive.jpg",
+    },
+]
 
 escolhe.addEventListener("click", () => {
     let aleatorio = Math.floor(Math.random() * album.length)
     let escolhido = album[aleatorio]
 
-    img.src = escolhido
+    img.src = escolhido.img
     res.textContent = "I É PRA ESCUTAR JOAO!"
-    a.textContent = "Escuta seu album legal ai dogão"
+    a.textContent = `Album escolhido para ser escutado: ${escolhido.nome}`
 })
