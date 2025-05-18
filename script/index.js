bdd18dd43dc30e315dad1d153c5a720e6c38013c
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-05-17 22:35:08 -0300

    fa

---
+++ script/index.js
@@ -0,0 +1,14 @@
+const escolhe = document.getElementById("btn")
+const res = document.getElementById("res")
+const img = document.getElementById("img")
+
+let album = ["imagens/angels.jpg", "imagens/fire.jpg", "imagens/holy.jpg", "imagens/mentalize.jpeg", "imagens/paradisum.jpg", "imagens/reason.jpg", "imagens/ritual.jpg", "imagens/ritualive.jpg", "imagens/soldiers.jpg", "imagens/theatre.jpg", "imagens/theturn.jpg", "imagens/time.jpg", "imagens/virgo.jpg", "imagens/tolive.jpg"]
+
+escolhe.addEventListener("click", () => {
+    let aleatorio = Math.floor(Math.random() * album.length)
+    let escolhido = album[aleatorio]
+
+    img.src = escolhido
+    img.style.display  = "block"
+    res.textContent = "Escuta Legal"
+})
